61a7ea70eda669ec8706dc16ec7c6dc42236a23b
Jet Yu <jetyu91@gmail.com>
2020-04-13 18:54:45 +0800

    1

---
+++ src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,10 @@
+package com.how2java.tmall.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.how2java.tmall.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+}
